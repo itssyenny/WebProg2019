c0d92e81e8e9d277b756de212623eecbe6a1f0d6
Syn <b05902102@ntu.edu.tw>
2019-03-16 17:11:30 +0800

    Update sketch.js

--- Homework02/sketch.js
+++ Homework02/sketch.js
@@ -5,7 +5,12 @@ const images = ["blue", "red", "yellow"].map(
 
 const scores_img = ["0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png",
                     "7.png", "8.png", "9.png"];
-let bgImg;
+
+const backgroundImg = ["assets/sprites/background-night.png", "assets/sprites/background-day.png"];
+
+// assets from: https://github.com/sourabhv/FlapPyBird/tree/master/assets
+
+let bgImg = [];
 let baseImg;                    
 let birdImg;
 let bird;
@@ -25,16 +30,13 @@ var point;
 var swoosh;
 var wing;
 var die;
-let x3, y3;
-let index, start = 0, end = 0;
+let imgID, index, start = 0, end = 0;
 
-// assets from: https://github.com/sourabhv/FlapPyBird/tree/master/assets
 function preload() {
     point = loadSound("assets/audio/point.ogg");
     swoosh = loadSound("assets/audio/swoosh.ogg");
     wing = loadSound("assets/audio/wing.ogg");
     die = loadSound("assets/audio/die.ogg");
-    bgImg = loadImage("assets/sprites/background-night.png");
     baseImg = loadImage("assets/sprites/base.png");
     birdImg = loadImage("assets/sprites/bluebird-midflap.png");
     index = Math.floor(Math.random() * (end - start)) + start;
@@ -43,6 +45,12 @@ function preload() {
     pipeDown = loadImage("assets/sprites/pipe-red-lower.png");
     readyImg = loadImage("assets/sprites/message.png");
     gameover = loadImage("assets/sprites/gameover.png");
+
+    var cnt = 0;
+    for(var i = 0; i < 2; i++) {
+        bgImg[cnt] = loadImage(backgroundImg[i]);
+        cnt++;
+    }
     for(var k = 0; k < scores_img.length; k++) {
         result[k] = loadImage("assets/sprites/" + scores_img[k]);
     }
@@ -74,16 +82,17 @@ function draw() {
     // Render function (called per frame.)
     background(0);
 
-    // x1 -= pipes[0].speed * 0.075;
     push();
     x1 -= 0.55;
-    if(x1 < -bgImg.width) {
-        x1 += bgImg.width;
+    if(x1 < -bgImg[imgID].width) {
+        x1 += bgImg[imgID].width;
     }
     x1 *= 1;
     translate(x1, y1);
-    image(bgImg, 0, 0, bgImg.width * bgScale, bgImg.height * bgScale);
-    image(bgImg, bgImg.width+100, 0, bgImg.width * bgScale, bgImg.height * bgScale);
+    // console.log('image ' + bgImg[imgID].width, bgImg[imgID].height);
+    // console.log('imgID ' + imgID);
+    image(bgImg[imgID], 0, 0, 288 * bgScale, 512 * bgScale);
+    image(bgImg[imgID], 288, 0, 288 * bgScale, 512 * bgScale);
     pop();
 
     if(frameCount % 100 == 0) {
@@ -176,7 +185,7 @@ function GameOver() {
 }
 
 function reset() {
-    // index = Math.floor(Math.random() * (1 - 0)) + 0;
+    imgID = Math.floor(Math.random() * (2 - 0)) + 0;
     if(end >= 9) {
         start = 0;
         end = 3;
@@ -189,7 +198,7 @@ function reset() {
     touched = false;
     x1 = 0; 
     y1 = 0; 
-    bgScale = width / bgImg.width;
+    bgScale = width / 288;
 
     x2 = 0; 
     y2 = 0;
@@ -232,7 +241,7 @@ function Bird() {
         push();
         translate(this.x, this.y);
         rotate(this.angle);
-        console.log('index bird = '+ index);
+        // console.log('index bird = '+ index);
         image(manybirds[index], 0, 0,34,24);
         pop();
     }
@@ -269,8 +278,8 @@ function Pipe() {
     this.bottom = height - (center + spacing / 2);
 
     this.x = width;
-    this.width = 80;
-    this.speed = 4;
+    this.width = 65;
+    this.speed = 3.5;
 
     this.hasCollided = false;
     this.passed = false;
